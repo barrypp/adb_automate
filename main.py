c1fed2b8b7e5a1014174388f80362ba8cf1e04dc
Barrypp.zzx <4166765+barrypp@users.noreply.github.com>
2022-07-27 10:52:17 +0800

    简化函数

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ dumpsys_activity = 'shell dumpsys activity a'
 screencap = 'exec-out screencap'
 wm_size = 'shell wm size'
 
-main_screen_setting_box = (1280, 2685, 1370, 2770)
-
 
 def run(para):
     return subprocess.check_output(var.adb_path + ' ' + para, shell=True)
@@ -33,56 +31,53 @@ def get_screen_size():
     return list(map(int, match.groups()))
 
 
-def check_current_activity(regstr=None):
+def get_current_activity():
     out = run(dumpsys_activity)
-    match = re.search(r".+mResumedActivity.+", out.decode())
+    match = re.search(r".+mResumedActivity.+ (com.+) ", out.decode())
     if (match):
-        if regstr is not None:
-            return (bool(re.search(regstr, match[0])), match[0])
-        else:
-            return (None, match[0])
-    else:
-        return (None, None)
+        return match.group(1)
 
 
-def wait_and_return_from_ads():
-    time.sleep(10)
-    while (not check_current_activity(var.game)[0]):
-        time.sleep(2)
-        run(input_keyevent+' 4')
-    time.sleep(1)
-
-
-def check_screen(box=None,im_template=None, p=0.9):
+def get_screen(box=None):
     im = np.frombuffer(run(screencap + ' -p'), dtype=np.uint8)
     im = cv2.imdecode(im, cv2.IMREAD_COLOR)
-    if (box):
-        im = im[box[1]:box[3], box[0]:box[2]]
-    if (im_template is not None):
-        out = cv2.matchTemplate(im, im_template, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(out)
-        return (max_val >= p, (min_val, max_val, min_loc, max_loc),im)
+    if (box is not None):
+        return im[box[1]:box[3], box[0]:box[2]]
     else:
-        return (None,None,im)
+        return im
 
 
-def check_ocr(im, str=None, p=0.9):
-    # this needs to run only once to load the model into memory
+def match_icon(im, im_temp, p=0.9):
+    out = cv2.matchTemplate(im, im_temp, cv2.TM_CCOEFF_NORMED)
+    return cv2.minMaxLoc(out)[1] >= p
+
+
+def search_icon(im, im_temp):
+    out = cv2.matchTemplate(im, im_temp, cv2.TM_CCOEFF_NORMED)
+    return cv2.minMaxLoc(out)
+
+
+def get_ocr(im):
     reader = easyocr.Reader(['ch_sim', 'en'])
-    ocr = reader.readtext(im)
-    if (str is not None):
-        for o in ocr:
-            if (o[1] == str and o[2] >= p):
-                return (True, o, ocr)
-        return (False, None, ocr)
-    else:
-        return (None, None, ocr)
+    return reader.readtext(im)
+
+
+def match_ocr(ocr, str, p=0.9):
+    for o in ocr:
+        if (o[1] == str and o[2] >= p):
+            return True
+
+
+def search_ocr(ocr, str, p=0.9):
+    for o in ocr:
+        if (o[1] == str and o[2] >= p):
+            return o
 
 
 def wait_and_check_ocr(str, p=0.9, box=None):
     while(True):
-        im = check_screen(box)[2]
-        if (check_ocr(im, str, p)[0]):
+        im = get_screen(box)
+        if (match_ocr(get_ocr(im), str, p)):
             return
         time.sleep(1)
 
@@ -101,6 +96,7 @@ def plot_ocr(im, text):
 
 
 def do_ads_1(count):
+    run(input_tap+' 840 2900')
     while(count):
         count -= 1
         run(input_tap+' 770 2500')
@@ -114,47 +110,57 @@ def do_ads_2(count):
         time.sleep(0.5)
         run(input_tap+' 1000 1700')
         run(input_tap+' 1000 1900')
-        if (check_current_activity(var.game)):
-            run(input_keyevent+' 4')
-            time.sleep(1)
+        time.sleep(0.5)
+        if (get_current_activity() == var.game):
+            try_switch_to_main_screen()
         else:
             wait_and_return_from_ads()
 
+
+def wait_and_return_from_ads():
+    time.sleep(10)
+    while (get_current_activity() != var.game):
+        time.sleep(2)
+        run(input_keyevent+' 4')
+    time.sleep(1)
+
+
 def try_switch_to_main_screen():
     im_temp = cv2.imread('template/main_screen_setting.png')
-    time.sleep(5)
-    while(not check_screen(main_screen_setting_box,im_temp)[0]):
+    time.sleep(2)
+    while(not match_icon(get_screen((1280, 2685, 1370, 2770)), im_temp)):
         run(input_keyevent+' 4')
         time.sleep(2)
 
 
-while (True):
-    out = run('devices')
-    match = re.search(var.ip, out.decode())
-    if (match):
-        break
-    run(f'connect {var.ip}:{var.port}')
-    time.sleep(1)
+if __name__ == "__main__":
 
-out = run(am_start + ' ' + var.game)
-run(pointer_location + ' 1')
+    while (True):
+        out = run('devices')
+        match = re.search(var.ip, out.decode())
+        if (match):
+            break
+        out = run(f'connect {var.ip}:{var.port}')
+        print(out.decode())
+        time.sleep(1)
 
+    out = run(am_start + ' ' + var.game)
+    run(pointer_location + ' 1')
 
-try_switch_to_main_screen()
+    try_switch_to_main_screen()
 
-# do_ads_1(10)
-# do_ads_2(6)
+    # do_ads_1(6)
+    # do_ads_2(6)
 
-#im = get_screen((1280, 2685, 1370, 2770))
-#cv2.imwrite('template/icon.png', im)
-#plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
-#plt.show()
+    #im = get_screen((1280, 2685, 1370, 2770))
+    #cv2.imwrite('template/icon.png', im)
+    #plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+    #plt.show()
 
-im = check_screen()[2]
-plot_ocr(im, check_ocr(im)[2])
+    im = get_screen()
+    plot_ocr(im, get_ocr(im))
 
-#img = cv2.imread
+    #img = cv2.imread
 
-
-run(pointer_location + ' 0')
-print('ok')
+    run(pointer_location + ' 0')
+    print('ok')
